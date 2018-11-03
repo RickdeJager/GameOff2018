bbdfc272ec2130fb2fdccb2c6b23a85a301cc078
TheFloatGoat <rickdejager99@gmail.com>
2018-11-03 20:18:04 +0100

    Simple platformer example

---
+++ scripts/movement.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const SPEED = 300
+const JUMP_SPEED = 500
+const GRAVITY = 17
+var motion = Vector2()
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _physics_process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	var nextMotion = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		nextMotion.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		nextMotion.x += 1
+	#Enter or space by default
+	elif Input.is_action_pressed("ui_accept") and is_on_floor():
+		nextMotion.y -= 1
+	else:
+		motion.x = 0
+	#Apply current motion
+	motion.x = SPEED * nextMotion.x
+	motion.y += JUMP_SPEED * nextMotion.y 
+	#Apply gravity
+	motion.y += GRAVITY
+	
+	motion = move_and_slide(motion, UP)
