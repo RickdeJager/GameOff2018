9ef49d491a9bb107b7571ad5578a1012326f3048
TheFloatGoat <rickdejager99@gmail.com>
2018-11-06 19:13:11 +0100

    Fixed sensors picking up ui elements

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 var screenSize
 var camera
 var root
+var beenRunning = true
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -10,16 +11,14 @@ func _ready():
 	camera = get_parent().get_node("Player/PlayerCamera")
 	root = get_tree().get_root().get_node("Root")
 	screenSize = get_viewport_rect().size
-	rect_position = screenSize/2
-	rect_size = screenSize
-	hide()
 
 func _process(delta):
 	if root.running:
-		hide()
+		if not beenRunning:
+			camera.offset = Vector2(0, 0)
+			beenRunning = true
 	else:
-		show()
-	var center = camera.get_camera_screen_center()
-	rect_position = center - screenSize/2
-	rect_size = screenSize
+		if beenRunning:
+			camera.offset = rect_global_position - camera.get_camera_screen_center() + screenSize/2
+			beenRunning = false
 
